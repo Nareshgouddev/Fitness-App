833c99bf2d90cbd027fa7243c21aa8dcc66209da
Naresh <ediganareshgoud111@gmail.com>
2026-04-27 00:11:07 +0530

    feat(user): create initial UserService structure
    - Add UserController with /api/users/{userId} and /register endpoints
    - Configure MariaDB database connection and Hibernate dialect in application.yml

---
+++ userService/src/main/java/com/Fitness/userService/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.Fitness.userService.controller;
+import com.Fitness.userService.dto.RegisterRequest;
+import com.Fitness.userService.dto.UserResponse;
+import com.Fitness.userService.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+    @GetMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(request.register(request));
+    }
+}
---
+++ userService/src/main/java/com/Fitness/userService/service/UserService.java
@@ -0,0 +1,7 @@
+package com.Fitness.userService.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
