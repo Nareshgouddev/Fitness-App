0ae42e4f31a388bb60e3805ec2205d3afabf29f7
Naresh <ediganareshgoud111@gmail.com>
2026-05-18 17:34:58 +0530

    Added the ActivityServices

---
+++ ActiveService/src/main/java/com/fitness/ActiveService/ActiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.ActiveService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveServiceApplication.class, args);
+	}
+
+}
